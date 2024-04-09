181192d7e333f7bca45f8277c028b75fbd10d8da
FelipeGuttemberg <fgos1@aluno.ifal.edu.br>
2024-04-09 15:44:23 -0300

    Versão Inicial

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button,} from 'react-native';
 import React from 'react';
 import { useState } from 'react';
 
@@ -9,7 +9,7 @@ export default function App() {
       <Text style={estilos.cadastro}>Faça o seu cadastro</Text>
 
       <View>
-      <TextInput style={estilos.email} placeholder='Digite o seu email'>  </TextInput>
+      <TextInput style={estilos.email} placeholder='Digite o seu email'> </TextInput>
       <TextInput style={estilos.nome} placeholder='Digite o seu nome de usuário'> </TextInput>
       <TextInput style={estilos.senha} placeholder='Digite o seu a sua senha'> </TextInput>
       <Button color={black} title='Finalizar Cadastro'></Button>
@@ -19,3 +19,7 @@ export default function App() {
   );
 }
 
+const estilos = StyleSheet.create({
+
+
+});
