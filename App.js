5fdb5b091b57ef93f59db76bcd6fce7ebfced3b9
FelipeGuttemberg <fgos1@aluno.ifal.edu.br>
2024-04-09 15:25:46 -0300

    Versão Inicial

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.teladeCadastro}>
+
+      <Text style={estilos.cadastro}>Faça o seu cadastro</Text>
+
+      <View>
+      <TextInput style={estilos.email} placeholder='Digite o seu email'>  </TextInput>
+      <TextInput style={estilos.nome} placeholder='Digite o seu nome de usuário'> </TextInput>
+      <TextInput style={estilos.senha} placeholder='Digite o seu a sua senha'> </TextInput>
+      <Button color={black} title='Finalizar Cadastro'></Button>
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
