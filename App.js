179686b9268f15ea008929e328cc28af90845cec
FelipeGuttemberg <fgos1@aluno.ifal.edu.br>
2024-04-09 16:29:14 -0300

    Versão Inicial 4

--- App.js
+++ App.js
@@ -9,10 +9,10 @@ export default function App() {
       <Text style={estilos.cadastro}>Faça o seu cadastro</Text>
 
       <View>
-      <TextInput style={estilos.email} placeholder='Digite o seu email'> </TextInput>
-      <TextInput style={estilos.nome} placeholder='Digite o seu nome de usuário'> </TextInput>
-      <TextInput style={estilos.senha} placeholder='Digite o seu a sua senha'> </TextInput>
-      <Button style={estilos.botao} color={black} title='Finalizar Cadastro'></Button>
+      <TextInput style={estilos.email} placeholder='Digite o seu email'/> 
+      <TextInput style={estilos.nome} placeholder='Digite o seu nome de usuário'/> 
+      <TextInput style={estilos.senha} placeholder='Digite o seu a sua senha'/> 
+      <Button style={estilos.botao} color='black' title='Finalizar Cadastro'></Button>
       </View>
 
     </View>
@@ -22,11 +22,12 @@ export default function App() {
 const estilos = StyleSheet.create({
 
 telaDeCadastro:{
-
+    flex: 1,
+    justifyContent: 'center'
 },
 
 cadastro:{
-
+  
 },
 
 email:{
